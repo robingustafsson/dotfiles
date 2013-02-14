ff6044ef29348d8ede5c8d9aab1354e1bbe3dcc5
Robin Gustafsson <i@robingustafsson.com>
2013-02-14 20:50:26 +0100

    Fixed bootstrap.sh to install pip and use anonymous github access.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,12 +5,12 @@ function notice { echo -e "\033[1;32m=> $1\033[0m"; }
 function error { echo -e "\033[1;31m=> Error: $1\033[0m"; }
 
 # Install dependencies.
-yes | sudo apt-get install build-essential git-core curl
+yes | sudo apt-get install build-essential git-core curl python-pip
 
 # Install ruby
 curl -L get.rvm.io | bash -s stable
 echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' >> ~/.bashrc
-. ~/.bashrc
+source ~/.bashrc
 rvm install 1.9.3
 rvm use 1.9.3
 rvm --default use 1.9.3
@@ -35,7 +35,7 @@ if [ -d ~/.dotfiles ]; then
   bundle exec rake install
 else
   notice "Downloading..."
-  git clone --recursive git@github.com:robingustafsson/dotfiles.git ~/.dotfiles
+  git clone --recursive git://github.com/robingustafsson/dotfiles.git ~/.dotfiles
 
   notice "Installing..."
   cd ~/.dotfiles
